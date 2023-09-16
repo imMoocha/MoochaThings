b84737c5bff6dfa53f44ed92aa5db919c8126ecd
Moocha <137570653+imMoocha@users.noreply.github.com>
2023-09-16 12:19:14 -0400

    Add files via upload

---
+++ numeros.js
@@ -0,0 +1,11 @@
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+
+if (num1 >= num2 && num1 >= num3) {
+  console.log("El número más grande es num1:", num1);
+} else if (num2 >= num1 && num2 >= num3) {
+  console.log("El número más grande es num2:", num2);
+} else {
+  console.log("El número más grande es num3:", num3);
+}
