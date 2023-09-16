281e40d54cb227a05f17670cbfc93b68eda7c5e7
Moocha <137570653+imMoocha@users.noreply.github.com>
2023-09-16 12:22:32 -0400

    Add files via upload

---
+++ operadores.js
@@ -0,0 +1,11 @@
+function cualEsMayor(num1, num2, num3) {
+  if (num1 >= num2 && num1 >= num3) {
+    return num1;
+  } else if (num2 >= num1 && num2 >= num3) {
+    return num2;
+  } else {
+    return num3;
+  }
+}
+
+console.log(cualEsMayor(1, 2, 3)); // Imprimirá el número más grande entre 1, 2 y 3
